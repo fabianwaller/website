8f9776ef2f98bb2e5e461c071da23dadf3adc57b
Fabian Waller <mail@fabianwaller.de>
2025-09-09 22:05:04 +0200

    cleanup

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -4,13 +4,8 @@ import * as React from "react";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-import dynamic from "next/dynamic";
 import { ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return (
-    <>
-      <NextThemesProvider {...props}>{children}</NextThemesProvider>
-    </>
-  );
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
