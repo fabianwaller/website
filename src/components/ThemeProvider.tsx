57258276196edbe72629a62a729ba4a9db198760
Fabian Waller <mail@fabianwaller.de>
2025-09-09 22:01:36 +0200

    fix theme flicker

--- src/components/ThemeProvider.tsx
+++ src/components/ThemeProvider.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import * as React from "react";
-const NextThemesProvider = dynamic(
-  () => import("next-themes").then((e) => e.ThemeProvider),
-  {
-    ssr: false,
-  },
-);
+
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 import dynamic from "next/dynamic";
 import { ThemeProviderProps } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    <>
+      <NextThemesProvider {...props}>{children}</NextThemesProvider>
+    </>
+  );
 }
